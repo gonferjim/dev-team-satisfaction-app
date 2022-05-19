d13b0ca4aa15e3bbf2355e6639d63b88d3f0a47a
Gonzalo <56033637+gonferjim@users.noreply.github.com>
2022-05-19 19:26:31 +0200

    Base structure from openai quickstart node

---
+++ pages/api/generate.js
@@ -0,0 +1,21 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  const completion = await openai.createCompletion("text-curie-001", {
+    prompt: generatePrompt(req.body.animal),
+    temperature: 0.0,
+  });
+  res.status(200).json({ result: completion.data.choices[0].text });
+}
+
+function generatePrompt(animal) {
+  const capitalizedAnimal =
+    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+  return `Suggest one name for an animal that is a superhero.
+  `;
+}
